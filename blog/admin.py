2d7d021097698d10286fc2671146233429e3fee5
Sarah Buettner <buettner-sarah@gmx.net>
2024-11-11 01:40:03 +0000

    feat: add more blog posts as .json & include filter options in admin view

--- blog/admin.py
+++ blog/admin.py
@@ -11,7 +11,7 @@ class BlogPostAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'slug', 'created_on')
     search_fields = ['title', 'content']
-    # list_filter = ('status',)
+    list_filter = ('author', 'created_on')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
