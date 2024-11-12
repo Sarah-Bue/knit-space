5ac7a8b39e41cde64f3f42662dbd3af7a5774ef9
Sarah Buettner <buettner-sarah@gmx.net>
2024-11-12 11:30:58 +0000

    fix: update links to home page

--- knit_space/settings.py
+++ knit_space/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-sarahbue-knitspace-dfcgalkmwni.ws.codeinstitute-ide.net','.herokuapp.com']
 
